cc3692b3a45678a2d780f09a26719b1e5a79e6b1
zain-7786 <mirzazain241@gmail.com>
2020-10-26 00:08:33 +0500

    radio-checkbox-assignment-commit-2

--- app/src/main/java/org/haqnawaz/a20201021/Main2Activity.java
+++ app/src/main/java/org/haqnawaz/a20201021/Main2Activity.java
@@ -13,6 +13,8 @@ public class Main2Activity extends AppCompatActivity {
 
     RadioGroup radioGroup,radioGroup2;
     RadioButton radioButton;
+    Checkbox chk1,chk2,chk3,chk4;
+    CheckboxButton checkboxButton;
     TextView textView;
 
     @Override
@@ -28,6 +30,33 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public void ShowSelection(View view) {
+
+        switch(view.getId()){
+            case R.id.chk1:
+                chk1.setChecked(true);
+                chk2.setChecked(false);
+                chk3.setChecked(false);
+                chk4.setChecked(false);
+                break;
+            case R.id.chk2:
+                chk2.setChecked(true);
+                chk3.setChecked(false);
+                chk1.setChecked(false);
+                chk4.setChecked(false);
+                break;
+            case R.id.chk3:
+                chk3.setChecked(true);
+                chk2.setChecked(false);
+                chk1.setChecked(false);
+                chk4.setChecked(false);
+                break;
+            case R.id.chk4:
+                chk4.setChecked(true);
+                chk2.setChecked(false);
+                chk1.setChecked(false);
+                chk3.setChecked(false);
+                break;
+        }
 
         int checked = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(checked);
